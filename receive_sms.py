b67a5aff42bb18b67a4fdc89197c0aebaca6ff58
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-05-06 20:30:46 +0100

    adding some details from the body of the message

--- receive_sms.py
+++ receive_sms.py
@@ -1,22 +1,22 @@
 import os
 from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-	resp = MessagingResponse()
-	
-	resp.message("The Robots are coming! Head for the hills!")
-	
-	return str(resp)
-	
+    number = request.form['Form']
+    message_body = request.form['Body']
+
+    resp = twiml.Response()
+    resp.message('Hello {}, you said: {}'.format(number, message_body))
+
+    return str(resp)
+
+
+
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
-	
-	
-
-
-
+    app.run(debug=True)
