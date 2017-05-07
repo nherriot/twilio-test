b828376f42a6f15374152e75ac1b7f539f63757c
Nicholas Herriot <Nicholas.Herriot@vodafone.com>
2017-05-08 00:03:34 +0100

    updating add and receive with print statements

--- receive_sms.py
+++ receive_sms.py
@@ -4,19 +4,28 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+# Twilio SMS route was at: https://demo.twilio.com/welcome/sms/reply/
+# Use NGROK for routing.
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-	resp = MessagingResponse()
-	
-	resp.message("The Robots are coming! Head for the hills!")
-	
-	return str(resp)
-	
+    print "We are in sms_reply"
+    print "The message is: {} ".format(request.form.get('Body'))
+    print "The message is form: {}".format(request.form.get('From'))
+    print "The message is from country: {} ".format(request.form.get('FromCountry'))
+
+
+
+    resp = MessagingResponse()
+
+    resp.message("The Robots are coming! Head for the hills! By Nicholas Herriot")
+
+    return str(resp)
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
-	
-	
+    app.run(debug=True)
+
+
 
 
 
